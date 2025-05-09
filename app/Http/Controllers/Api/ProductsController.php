da4facd495a553a3d0538daa2527182b3be09f1f
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-05-10 00:06:46 +0700

    from Yoeung Yeng update crud

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -61,7 +61,7 @@ class ProductsController extends Controller
                 'description' => $request->description,
                 'price' => $request->price,
                 'qty' => $request->qty,
-                'image' => $image_url,	
+                'image' => $image_url,
             ]);
 
             return response()->json([
@@ -83,7 +83,19 @@ class ProductsController extends Controller
      */
     public function show(string $id)
     {
-        //
+        // get product by id
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product not found',
+            ], 404);
+        }
+        return response()->json([
+            'status' => true,
+            'message' => 'Product retrieved successfully',
+            'data' => $product
+        ], 200);
     }
 
     /**
@@ -91,7 +103,62 @@ class ProductsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // validate request
+        try {
+            $product = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'description' => 'required|string|max:255',
+                'price' => 'required|numeric',
+                'qty' => 'required|integer',
+                'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+            // check if validation fails
+            if ($product->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validation failed',
+                    'errors' => $product->errors()
+                ], 422);
+            }
+
+            // get product by id
+            $product = Product::find($id);
+            if (!$product) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Product not found',
+                ], 404);
+            }
+
+            // Save image to database
+            if ($request->hasFile('image')) {
+                $image_path = $request->file('image')->store('product/image', 'public'); // Store in storage/app/public/images
+                $image_url = asset('storage/' . $image_path); // Convert to URL
+            } else {
+                $image_url = $product->image;
+            }
+
+            // update product
+            $product->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'price' => $request->price,
+                'qty' => $request->qty,
+                'image' => $image_url,
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Product updated successfully',
+                'data' => $product
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product update failed',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -99,6 +166,20 @@ class ProductsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        // delete product by id
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Product not found',
+            ], 404);
+        }
+        // delete product
+        $product->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfully',
+        ], 200);
+
     }
 }
