1dc088e98819d25f0ac98453b5703f9cd63c2ea5
Phen-Reaksmey <reaksmeyphen777@gmail.com>
2025-05-10 09:05:51 +0700

    commit update security about jwt

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    // register
+    public function register(Request $request)
+    {
+        // validate the request
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // if validation fails, return error response
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'data' => $validator->errors()
+            ], 422);
+        }
+
+        // create user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        $token = JWTAuth::fromUser($user);
+        // return success response
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'token' => $token
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post("/register", [AuthController::class, 'register']);
 
-Route::apiResource('/products', ProductsController::class);
+
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('/products', ProductsController::class);
+});
